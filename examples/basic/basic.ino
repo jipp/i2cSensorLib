6723cecf03a1c4527af21b2f9775ac459a74c216
jipp <wolfgang.keller@wobilix.de>
2018-07-02 16:14:38 +0200

    updated for new methods

--- examples/basic/basic.ino
+++ examples/basic/basic.ino
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 
 #include <Wire.h>
-
 #include <i2cSensorLib.h>
 
 ADC_MODE(ADC_VCC);
@@ -17,35 +16,35 @@ void publishValues() {
   if (vcc.isAvailable) {
     vcc.getValues();
     Serial.print("VCC:    voltage: ");
-    Serial.println(vcc.voltage);
+    Serial.println(vcc.get(Sensor::VOLTAGE_MEASUREMENT));
   }
   if (bh1750.isAvailable) {
     bh1750.getValues();
     Serial.print("BH1750: illuminance: ");
-    Serial.println(bh1750.illuminance);
+    Serial.println(bh1750.get(Sensor::ILLUMINANCE_MEASUREMENT));
   }
   if (sht3x.isAvailable) {
     sht3x.getValues();
     Serial.print("SHT3X:  temperature: ");
-    Serial.print(sht3x.temperature);
+    Serial.print(sht3x.get(Sensor::TEMPERATURE_MEASUREMENT));
     Serial.print(" humidity: ");
-    Serial.println(sht3x.humidity);
+    Serial.println(sht3x.get(Sensor::HUMIDITY_MEASUREMENT));
   }
   if (bmp180.isAvailable) {
     bmp180.getValues();
     Serial.print("BMP180: temperature: ");
-    Serial.print(bmp180.temperature);
+    Serial.print(bmp180.get(Sensor::TEMPERATURE_MEASUREMENT));
     Serial.print(" pressure: ");
-    Serial.println(bmp180.pressure);
+    Serial.println(bmp180.get(Sensor::PRESSURE_MEASUREMENT));
   }
   if (bme280.isAvailable) {
     bme280.getValues();
     Serial.print("BME280: temperature: ");
-    Serial.print(bme280.temperature);
+    Serial.print(bme280.get(Sensor::TEMPERATURE_MEASUREMENT));
     Serial.print(" humidity: ");
-    Serial.print(bme280.humidity);
+    Serial.print(bme280.get(Sensor::HUMIDITY_MEASUREMENT));
     Serial.print(" pressure: ");
-    Serial.println(bme280.pressure);
+    Serial.println(bme280.get(Sensor::PRESSURE_MEASUREMENT));
   }
 }
 
