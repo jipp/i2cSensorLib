7302e47c58059afdd2861d3ff3d74b057fecb7d4
jipp <wolfgang.keller@wobilix.de>
2019-03-20 14:20:40 +0100

    added memory class

---
+++ src/Memory.cpp
@@ -0,0 +1,38 @@
+#include <Memory.h>
+
+Memory::Memory()
+{
+}
+
+void Memory::begin()
+{
+#if defined(ESP32)
+    this->isAvailable = true;
+#elif defined(ESP8266)
+    this->isAvailable = true;
+#else
+    this->isAvailable = false;
+#endif
+}
+
+void Memory::getValues()
+{
+#if defined(ESP32)
+    this->memory = ESP.getFreeHeap();
+#elif defined(ESP8266)
+    this->memory = ESP.getFreeHeap();
+#else
+    this->memory = NAN;
+#endif
+}
+
+float Memory::get(uint8_t Measurement)
+{
+    switch (Measurement)
+    {
+    case MEMORY_MEASUREMENT:
+        return this->memory;
+    default:
+        return NAN;
+    }
+}
